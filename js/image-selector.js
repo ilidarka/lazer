7cd493c7715b28f8d05b04c4da9dcb7116d102f1
Vladik Leshko <leshkovladik@gmail.com>
2023-02-06 09:56:45 +0300

    file upload

---
+++ js/image-selector.js
@@ -0,0 +1,50 @@
+/******* */
+
+function init() {
+	document.getElementById("clickMe1").addEventListener("click", function() {
+		document.getElementById("lazer-display1").style.display = "block";
+        document.getElementById("clickMe1").classList.add('selected');
+        document.getElementById("clickMe2").classList.remove('selected');
+        document.getElementById("clickMe3").classList.remove('selected');
+        document.getElementById("clickMe4").classList.remove('selected');
+		document.getElementById("lazer-display3").style.display = "none";
+		document.getElementById("lazer-display4").style.display = "none";
+		document.getElementById("lazer-display2").style.display = "none";
+	});
+
+	document.getElementById("clickMe2").addEventListener("click", function() {
+		document.getElementById("lazer-display2").style.display ="block";
+        document.getElementById("clickMe2").classList.add('selected');
+        document.getElementById("clickMe1").classList.remove('selected');
+        document.getElementById("clickMe3").classList.remove('selected');
+        document.getElementById("clickMe4").classList.remove('selected');
+		document.getElementById("lazer-display1").style.display ="none";
+		document.getElementById("lazer-display3").style.display ="none";
+		document.getElementById("lazer-display4").style.display ="none";
+	
+	});
+
+	document.getElementById("clickMe3").addEventListener("click", function() {
+		document.getElementById("lazer-display3").style.display ="block";
+        document.getElementById("clickMe3").classList.add('selected');
+        document.getElementById("clickMe1").classList.remove('selected');
+        document.getElementById("clickMe2").classList.remove('selected');
+        document.getElementById("clickMe4").classList.remove('selected');
+		document.getElementById("lazer-display1").style.display ="none";
+		document.getElementById("lazer-display4").style.display ="none";
+		document.getElementById("lazer-display2").style.display ="none";
+	});
+
+	document.getElementById("clickMe4").addEventListener("click", function() {
+		document.getElementById("lazer-display4").style.display ="block";
+        document.getElementById("clickMe4").classList.add('selected');
+        document.getElementById("clickMe1").classList.remove('selected');
+        document.getElementById("clickMe2").classList.remove('selected');
+        document.getElementById("clickMe3").classList.remove('selected');
+		document.getElementById("lazer-display1").style.display ="none";
+		document.getElementById("lazer-display3").style.display ="none";
+		document.getElementById("lazer-display2").style.display ="none";
+	});
+}
+init();
+
